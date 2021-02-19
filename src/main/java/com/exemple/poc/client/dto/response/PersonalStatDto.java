da9351dab84b9ebaef25d6ebe7a8785d81087b01
Jérémy Truquet <jeremy.truquet-ISC_France@fr.ibm.com>
2021-02-19 16:44:15 +0100

    Comit

---
+++ src/main/java/com/exemple/poc/client/dto/response/PersonalStatDto.java
@@ -0,0 +1,22 @@
+package com.exemple.poc.client.dto.response;
+
+public class PersonalStatDto {
+	
+	private String lastname;
+	private Double pointsAvg;
+	
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	public Double getPointsAvg() {
+		return pointsAvg;
+	}
+	public void setPointsAvg(Double pointsAvg) {
+		this.pointsAvg = pointsAvg;
+	}
+
+
+}
