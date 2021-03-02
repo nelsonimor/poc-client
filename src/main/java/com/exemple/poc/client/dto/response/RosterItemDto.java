6c9d18942b8eb0cb1c69226893a7ef22a417baa1
jeremChapelle <jeremtruc31@hotmail.fr>
2021-03-02 21:35:21 +0100

    Update rosterItem dto (add roster info)

--- src/main/java/com/exemple/poc/client/dto/response/RosterItemDto.java
+++ src/main/java/com/exemple/poc/client/dto/response/RosterItemDto.java
@@ -8,6 +8,10 @@ public class RosterItemDto {
 	private PersonDTO personDto;
 	private Date startdate;
 	private Date enddate;
+	private String teamName;
+	private Date seasonStartdate;
+	private Date seasonEnddate;
+	private Integer rosterId;
 	
 	public Date getStartdate() {
 		return startdate;
@@ -26,6 +30,30 @@ public class RosterItemDto {
 	}
 	public void setPersonDto(PersonDTO personDto) {
 		this.personDto = personDto;
+	}
+	public String getTeamName() {
+		return teamName;
+	}
+	public void setTeamName(String teamName) {
+		this.teamName = teamName;
+	}
+	public Date getSeasonStartdate() {
+		return seasonStartdate;
+	}
+	public void setSeasonStartdate(Date seasonStartdate) {
+		this.seasonStartdate = seasonStartdate;
+	}
+	public Date getSeasonEnddate() {
+		return seasonEnddate;
+	}
+	public void setSeasonEnddate(Date seasonEnddate) {
+		this.seasonEnddate = seasonEnddate;
+	}
+	public Integer getRosterId() {
+		return rosterId;
+	}
+	public void setRosterId(Integer rosterId) {
+		this.rosterId = rosterId;
 	}
 
 
