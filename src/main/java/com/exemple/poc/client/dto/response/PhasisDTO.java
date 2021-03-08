dcf377e25ba15b467daf05eedd67ca3d930e88c5
Jérémy Truquet <jeremy.truquet-ISC_France@fr.ibm.com>
2021-03-08 13:47:15 +0100

    Add phasisDTO

---
+++ src/main/java/com/exemple/poc/client/dto/response/PhasisDTO.java
@@ -0,0 +1,34 @@
+package com.exemple.poc.client.dto.response;
+
+public class PhasisDTO {
+
+	private int id;
+	private String name;
+	private int competitionId;
+	private String competitionName;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getCompetitionId() {
+		return competitionId;
+	}
+	public void setCompetitionId(int competitionId) {
+		this.competitionId = competitionId;
+	}
+	public String getCompetitionName() {
+		return competitionName;
+	}
+	public void setCompetitionName(String competitionName) {
+		this.competitionName = competitionName;
+	}
+}
