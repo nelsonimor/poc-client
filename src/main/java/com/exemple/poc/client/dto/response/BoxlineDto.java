260c3425bfddafd6eae08f8dbda494db7d9edef2
Jérémy Truquet <JeremyTruquet@9.145.159.21>
2021-03-15 17:30:05 +0100

    Up competition

--- src/main/java/com/exemple/poc/client/dto/response/BoxlineDto.java
+++ src/main/java/com/exemple/poc/client/dto/response/BoxlineDto.java
@@ -8,6 +8,25 @@ public class BoxlineDto {
 	private int points;
 	private String teamname;
 	
+	private Integer totalShoot;
+	private Integer totalShootAttempts;
+	private Integer threePointShoot;
+	private Integer threePointShootAttempts;
+	private Integer freeThrow;
+	private Integer freeThrowAttempts;
+	private Integer rebound;
+	private Integer reboundOffensive;
+	private Integer reboundDefensive;
+	private Integer assist;
+	private Integer steal;
+	private Integer turnover;
+	private Integer block;
+	private Integer ranking;
+	private Integer foulsPersonal;
+	private Integer foulsProvoked;
+	private Integer minutes;
+	private boolean starter;
+	
 	public int getId() {
 		return id;
 	}
@@ -37,6 +56,115 @@ public class BoxlineDto {
 	}
 	public void setTeamname(String teamname) {
 		this.teamname = teamname;
+	}
+	public Integer getTotalShoot() {
+		return totalShoot;
+	}
+	public void setTotalShoot(Integer totalShoot) {
+		this.totalShoot = totalShoot;
+	}
+	public Integer getTotalShootAttempts() {
+		return totalShootAttempts;
+	}
+	public void setTotalShootAttempts(Integer totalShootAttempts) {
+		this.totalShootAttempts = totalShootAttempts;
+	}
+
+	public Integer getFreeThrow() {
+		return freeThrow;
+	}
+	public void setFreeThrow(Integer freeThrow) {
+		this.freeThrow = freeThrow;
+	}
+	public Integer getFreeThrowAttempts() {
+		return freeThrowAttempts;
+	}
+	public void setFreeThrowAttempts(Integer freeThrowAttempts) {
+		this.freeThrowAttempts = freeThrowAttempts;
+	}
+	public Integer getRebound() {
+		return rebound;
+	}
+	public void setRebound(Integer rebound) {
+		this.rebound = rebound;
+	}
+	public Integer getReboundOffensive() {
+		return reboundOffensive;
+	}
+	public void setReboundOffensive(Integer reboundOffensive) {
+		this.reboundOffensive = reboundOffensive;
+	}
+	public Integer getReboundDefensive() {
+		return reboundDefensive;
+	}
+	public void setReboundDefensive(Integer reboundDefensive) {
+		this.reboundDefensive = reboundDefensive;
+	}
+	public Integer getAssist() {
+		return assist;
+	}
+	public void setAssist(Integer assist) {
+		this.assist = assist;
+	}
+	public Integer getSteal() {
+		return steal;
+	}
+	public void setSteal(Integer steal) {
+		this.steal = steal;
+	}
+	public Integer getTurnover() {
+		return turnover;
+	}
+	public void setTurnover(Integer turnover) {
+		this.turnover = turnover;
+	}
+	public Integer getBlock() {
+		return block;
+	}
+	public void setBlock(Integer block) {
+		this.block = block;
+	}
+	public Integer getRanking() {
+		return ranking;
+	}
+	public void setRanking(Integer ranking) {
+		this.ranking = ranking;
+	}
+	public Integer getFoulsPersonal() {
+		return foulsPersonal;
+	}
+	public void setFoulsPersonal(Integer foulsPersonal) {
+		this.foulsPersonal = foulsPersonal;
+	}
+	public Integer getFoulsProvoked() {
+		return foulsProvoked;
+	}
+	public void setFoulsProvoked(Integer foulsProvoked) {
+		this.foulsProvoked = foulsProvoked;
+	}
+	public Integer getMinutes() {
+		return minutes;
+	}
+	public void setMinutes(Integer minutes) {
+		this.minutes = minutes;
+	}
+	public boolean isStarter() {
+		return starter;
+	}
+	public void setStarter(boolean starter) {
+		this.starter = starter;
+	}
+	public Integer getThreePointShoot() {
+		return threePointShoot;
+	}
+	public void setThreePointShoot(Integer threePointShoot) {
+		this.threePointShoot = threePointShoot;
+	}
+	public Integer getThreePointShootAttempts() {
+		return threePointShootAttempts;
+	}
+	public void setThreePointShootAttempts(Integer threePointShootAttempts) {
+		this.threePointShootAttempts = threePointShootAttempts;
 	}
 
 }
--- src/main/java/com/exemple/poc/client/dto/response/CompetitionDto.java
+++ src/main/java/com/exemple/poc/client/dto/response/CompetitionDto.java
@@ -1,11 +1,16 @@
 package com.exemple.poc.client.dto.response;
 
+import java.util.List;
+
 public class CompetitionDto {
 	
 	private int id;
 	private String name;
 	private String contient;
 	private String country;
+	private int nbOrgnizations;
+	private List<CompetitionOrganizationDto> competitionOrganizations;
+
 	
 	public int getId() {
 		return id;
@@ -30,6 +35,18 @@ public class CompetitionDto {
 	}
 	public void setCountry(String country) {
 		this.country = country;
+	}
+	public int getNbOrgnizations() {
+		return nbOrgnizations;
+	}
+	public void setNbOrgnizations(int nbOrgnizations) {
+		this.nbOrgnizations = nbOrgnizations;
+	}
+	public List<CompetitionOrganizationDto> getCompetitionOrganizations() {
+		return competitionOrganizations;
+	}
+	public void setCompetitionOrganizations(List<CompetitionOrganizationDto> competitionOrganizations) {
+		this.competitionOrganizations = competitionOrganizations;
 	}
 
 }
